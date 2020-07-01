d445cda3998617bef54f9206673ed6fb98496364
anbatina <43778631+anbatina@users.noreply.github.com>
2020-07-01 15:38:55 +0200

    Inicijalna verzija
    Pocetak

---
+++ Inicijalna verzija.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <title>pocetak</title>
+</head>
+<body>
+<div>
+    <h1>Inicijalna verzija</h1>
+</div>
+</body>
+</html>
